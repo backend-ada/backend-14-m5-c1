ce9c551836dc81cb9210d9951072da3192725348
Matías Arno <amatiasarno@gmail.com>
2023-09-08 10:38:52 -0300

    second commit

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,23 @@ import http from 'node:http';
 const PORT = 36000;
 
 const server = http.createServer((req, res) => {
-    console.log(req.url);    
-    console.log(req.headers);    
-    console.log(req.method);
-    console.log(req.statusCode);    
-	res.end('Hello world!');
+	const { url, method } = req;
+
+	if (url === '/') {
+		console.log('MAIN URL');
+		console.log(method);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.end('{"message": "Welcome to the main"}');
+	}
+
+	if (url === '/api') {
+		console.log('API URL');
+		console.log(method);
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json; charset=utf-8');
+		res.end('{"message": "Welcome to the API"}');
+	}
 });
 
 server.listen(PORT, () => {
